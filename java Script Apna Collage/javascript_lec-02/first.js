c19b29df6924d9e98dff71cad540de51e47b5fbf
mahesh <maheshsingh70780@gmail.com>
2026-09-12 08:52:22 +0530

    Add javascript lec 02

---
+++ java Script Apna Collage/javascript_lec-02/first.js
@@ -0,0 +1,29 @@
+// Revision is completed.
+console.log("Mahesh Singhjavascriot lecture 02is started");
+
+//Variable declaration in javascript (js).
+
+// Variables --> variables are containers for data.
+// Reserved keyword in java script --> console not made variable --> because it is reserved keyword in javascript kyuki ye --> pehle hi use ho chuka hai as console.log();
+
+// Types of Variables in JavaScript : Three-(3) types.
+//var --> Global variable.
+//let --> local variable.
+//const --> fixed or static variable.
+// let ka use as --> { let a = 10; let a = 20; console.log(a) } --> error dega kyuki let variable ko re-declare nahi kar sakte hain in same block. 
+// let as --> { let a = 10; { let a = 20; console.log(a) } } --> 20 print hoga kyuki inner block me re-declare kiya gya hai.
+// 
+
+// #Datatype in javascript :- it is defined as the which type of data stored by variable is called datatypes.
+// it is classified into two types:-
+//  1).Primitive Datatypes:- it is used to store simple values. & it is fixed in size. eg. Number, String, Boolean, Null, Undefined, BigInt, Symbol.
+//  2).Non-Primitive Datatypes :- it is used to store complex values. eg. objects, arrays, functions, etc.
+//  Number, String, Boolean, Null, Undefined, BigInt, Symbol --> Primitive Datatypes.
+//  
+// used in chrome to see datatype of a variable as  --> typeof variableName; --> it will return the datatype of a variable. eg. typeof a; --> it will return number if a = 10;
+// typeof name --> 'String' & typeof price = number;
+// object --> it is the collection of data in key-values.
+// eg. student as object --> { name: "Mayank", age: 19, rollno: 2402309073, cgpa: 8.2, ispass: true, isfail: false } --> it is a collection of data in key-values.
+//  name hai string, age hai number, rollno hai number, cgpa hai number, ispass hai boolean, isfail hai boolean.
+// Note :- let --> update and re-declare ho sakta hai
+//  const --> no updation nahi ho sakta update pr iskee object ki keys ko change kr sakte hain hm.
