4ada2a78bfb6cd21366901ae0fbd1b3beaff8a55
mahesh <maheshsingh70780@gmail.com>
2026-09-18 21:15:11 +0530

    Add JavaScript lecture folders

---
+++ javascript_Lec-02/script.js
@@ -0,0 +1,21 @@
+//Revision of Operaters and Conditional Statements is start
+console.log("Revision of operators and conditional statements");  //this code is printed Revision of operators and conditional statements -->
+//comment -->  ase bhi comment kar sakte h
+
+//Operater in java script
+//used to perform some operater on
+//#Arithmetic operater|| +,-,*, /||#Uranary operators
+//#Assignment operater||         || #Comparison Operators
+//#Relational Operators||
+
+
+let m =10;
+let n =12;
+//let c = m + n;
+console.log("m =",m,"&& n =",n);
+console.log("m + n =", m + n);
+console.log("m - n =", m - n);
+console.log("m * n =", m * n);
+console.log("m / n =", m / n);
+console.log("m % n =", m % n); //modulu oerater --> %
+console.log("m ** n =", m ** n);  //Exponential perater --> ** -->10^12
