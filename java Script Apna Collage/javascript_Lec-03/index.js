77f5983b672df80203e59f02db2b0e5eb47d1cb2
mahesh <maheshsingh70780@gmail.com>
2026-09-20 21:14:24 +0530

    Update JavaScript Lec 03

--- java Script Apna Collage/javascript_Lec-03/index.js
+++ java Script Apna Collage/javascript_Lec-03/index.js
@@ -1,5 +1,22 @@
-console.log("hello man");
-console.log("hello man");
-console.log("hello man");
-console.log("hello man");
-console.log("hello man");
+V// Revision of JS-Lec-03
+
+console.log("Loop in JavaScript");
+
+// 1). Loops
+
+// Loops are used to execute a piece of code again and again.
+
+// There are four types of loops in JavaScript:
+// 1). for Loop
+// 2). while Loop
+// 3). do while Loop
+// 4). forEach Loop
+
+// For Loop Syntax:
+// for (initialization; condition; increment / decrement)
+
+for (let i = 1; i <= 10; i++) {
+    console.log("Apna COLLAGE");
+}
+
+// Print 1 to 5.s
